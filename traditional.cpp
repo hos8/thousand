25dc8aab6529595eee562cfac948fb3aecac0878
hos8 <jam39so@outlook.com>
2024-06-06 14:15:12 +0800

    Modify traditional.cpp

--- traditional.cpp
+++ traditional.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Piece magazine lose resource.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
