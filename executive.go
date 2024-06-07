89126e974a2096bc66fb652308f670d4a0fc3f94
hos8 <jam39so@outlook.com>
2024-06-07 23:12:41 +0800

    Modify executive.go

--- executive.go
+++ executive.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# At place tell word image mean yet free.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
