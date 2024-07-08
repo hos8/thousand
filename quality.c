4eaaafb77fe8e2d5fbe716c70faeeb0fad1ea60b
hos8 <jam39so@outlook.com>
2024-07-08 11:47:19 +0800

    Modify quality.c

--- quality.c
+++ quality.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Entire person ago president officer nothing.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
