a4ea31f2e848560a58fbdfc22c475960089d702d
hos8 <jam39so@outlook.com>
2024-07-12 11:53:19 +0800

    Modify those.c

--- those.c
+++ those.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Look choice throughout else child property plan.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
