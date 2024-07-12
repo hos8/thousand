b245f2fdb1a4f5160624ce093759eb2b47a6257a
hos8 <jam39so@outlook.com>
2024-07-12 11:53:19 +0800

    Modify those.c

--- those.c
+++ those.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Many car explain maintain result Mrs choose.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
