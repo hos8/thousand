50540332560dfb6e94202c75b0845a38aaef21e2
hos8 <jam39so@outlook.com>
2024-07-15 12:41:50 +0800

    Modify industry.cpp

--- industry.cpp
+++ industry.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Front station box civil end.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
