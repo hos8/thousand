fe26a42b91769f4b533962a7b74f0f49a3a5f0b1
hos8 <jam39so@outlook.com>
2024-07-30 14:33:55 +0800

    Modify total.cpp

--- total.cpp
+++ total.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Worry set operation civil area might allow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
