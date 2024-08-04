56cf52c4d177a85b8d2c0e8f7dac387fa1b9d040
hos8 <jam39so@outlook.com>
2024-08-04 12:39:04 +0800

    Modify black.c

--- black.c
+++ black.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Job cup approach future start however learn.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
