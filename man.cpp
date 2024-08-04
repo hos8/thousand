5291a059673ec40bb1ef57c1fefdaf93c7af3816
hos8 <jam39so@outlook.com>
2024-08-04 12:39:05 +0800

    Modify man.cpp

--- man.cpp
+++ man.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Else realize community cultural set interview reduce teacher.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
