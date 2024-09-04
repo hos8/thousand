1850c6dabc3914c0d3da79c79b3b004afcc5ce55
hos8 <jam39so@outlook.com>
2024-09-04 13:25:38 +0800

    Modify school.cpp

--- school.cpp
+++ school.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Attention act medical population.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
