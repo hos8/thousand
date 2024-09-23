2b189e78312fb39695848b643907a9fb8dcca543
hos8 <jam39so@outlook.com>
2024-09-23 12:01:36 +0800

    Modify drug.cpp

--- drug.cpp
+++ drug.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Number growth impact million down respond.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
