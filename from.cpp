55a107deaafe6959423e867db6c2a287c06b16ba
hos8 <jam39so@outlook.com>
2024-10-09 10:50:29 +0800

    Modify from.cpp

--- from.cpp
+++ from.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Center name avoid his many smile two.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
