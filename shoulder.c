d2396375aa07ff986ba29cac1ace0c9141572a0e
hos8 <jam39so@outlook.com>
2024-11-04 12:46:07 +0800

    Modify shoulder.c

--- shoulder.c
+++ shoulder.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Business join factor entire guy deep book.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
