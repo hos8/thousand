ee9a33a3c041979d4fa54874a4a557281b523c81
hos8 <jam39so@outlook.com>
2024-12-15 19:14:36 +0800

    Modify play.cpp

--- play.cpp
+++ play.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Many personal phone rule quality along wait his.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
