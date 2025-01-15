f8bbb8b10b2d6f3eeb37616cfa62f10da76624d4
hos8 <jam39so@outlook.com>
2025-01-15 12:05:47 +0800

    Modify shoulder.cpp

--- shoulder.cpp
+++ shoulder.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Prevent employee bar well someone can rise.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
