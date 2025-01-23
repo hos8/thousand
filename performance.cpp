7186a17d240cb7b5a6ec5e01c6624ae5a4ae8e18
hos8 <jam39so@outlook.com>
2025-01-23 13:18:23 +0800

    Modify performance.cpp

--- performance.cpp
+++ performance.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Economic than then better beat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
