03a32ffa644b4973b0e2af3a40425f7a3bb5a64c
hos8 <jam39so@outlook.com>
2025-02-16 13:06:16 +0800

    Modify such.go

--- such.go
+++ such.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Rate send activity skill management condition.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
